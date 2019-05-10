f45e0e0391f56a40ef1e750ad985ea238a8d379c
hwse <hagenweisse@gmail.com>
2019-05-10 11:05:31 +0200

    findgrep

--- include.sh
+++ include.sh
@@ -1,4 +1,16 @@
+#!/usr/bin/env bash
 
 # Python virtualenv aliases
 alias acvenv='. venv/bin/activate'
 alias mkvenv='virtualenv -p /usr/bin/python3 venv'
+
+# search for expression in specified files
+findgrep() {
+	if [[ $1 = "-h" ]]; then
+		echo "findgrep [findexprt] [grepexpr]"
+	else
+		local findexpr=$1
+		local grepexpr=$2
+		find . -name "$findexpr" -exec grep -H "$grepexpr" {} \;
+	fi
+}
